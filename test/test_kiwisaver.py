ac674efff3d5c4f82faeae222b562bbc1e136aa3
anuj_ <anuj576@gmail.com>
2020-05-06 18:42:27 +1200

    add browser args

--- test/test_kiwisaver.py
+++ test/test_kiwisaver.py
@@ -39,6 +39,8 @@ class KiwiSaverCalculator(unittest.TestCase):
 
     def browser_options(self, options):
         options.add_argument("--headless")
+        options.add_argument("window-size=1920x1480")
+        options.add_argument('--disable-dev-shm-usage')
         options.add_argument("--no-sandbox")
         options.add_argument("disable-gpu")
 
