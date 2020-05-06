06bfd62354cbdd0e4179c60a567637fc8890a2a5
anuj_ <anuj576@gmail.com>
2020-05-06 13:44:53 +1200

    Don't start the test in headless mode

--- test/test_kiwisaver.py
+++ test/test_kiwisaver.py
@@ -14,9 +14,7 @@ class KiwiSaverCalculator(unittest.TestCase):
         :return:
         """
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--headless")
         chrome_options.add_argument("disable-gpu")
         self.driver = webdriver.Chrome(options=chrome_options)
 
